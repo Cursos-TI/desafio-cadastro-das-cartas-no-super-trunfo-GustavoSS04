e2a87e0c7efb4fd7277ec448fd9af37bae4c8b4e
GustavoSS04 <gustavo_s_souza@yahoo.com.br>
2025-02-11 21:38:00 +0000

    Codigo incial

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,6 +20,9 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+// codigo inicial 
+
+
 float Populacao;
 int Pontosturisticos;
 float Area;
